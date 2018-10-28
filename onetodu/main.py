5295a4d2b48d79c31e321dc4bf7263040582f848
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2018-10-28 18:03:46 +0900

    Add ToDo list keep feature

--- onetodu/main.py
+++ onetodu/main.py
@@ -1,3 +1,7 @@
+import json
+import pathlib
+import sys
+
 from kivy.app import App
 from kivy.core.text import LabelBase
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -11,8 +15,36 @@ LabelBase.register("NotoSansCJKjp-DemiLight",
 Config.set('graphics', 'width', '360')
 Config.set('graphics', 'height', '640')
 
+DATA_DIR_NAME = "data"
+DATA_FILE_NAME = "todo_list"
+DATA_FILE_PATH = pathlib.Path(
+    sys.argv[0]).parent / DATA_DIR_NAME / DATA_FILE_NAME
 
-class ToDoListItem(BoxLayout):
+
+class TodoListManager:
+    def __init__(self, manager_user):
+        self.todo_list = []
+        self.manager_user = manager_user
+
+    def add_todo(self, todo_text: str):
+        self.todo_list.append(todo_text)
+        self.manager_user.update_list(self.todo_list)
+
+    def remove_todo_by_text(self, todo_text: str):
+        self.todo_list.remove(todo_text)
+        self.manager_user.update_list(self.todo_list)
+
+    def save_to_json(self, file_path):
+        with open(file_path, "w") as file_obj:
+            json.dump(self.todo_list, file_obj)
+
+    def load_list_from_json(self, file_path):
+        with open(file_path, "r") as file_obj:
+            self.todo_list = json.load(file_obj)
+        self.manager_user.update_list(self.todo_list)
+
+
+class ToDoListItemWidget(BoxLayout):
     label = ObjectProperty(None)
 
     def __init__(self, label_text, **kwargs):
@@ -24,12 +56,13 @@ class MainScreen(Screen):
     todo_list = ObjectProperty(None)
     new_todo_text_input = ObjectProperty(None)
 
-    def add_todo(self, todo_text):
-        if todo_text:
-            self.todo_list.add_widget(ToDoListItem(label_text=todo_text))
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-    def remove_todo(self, todo_list_item_widget):
-        self.todo_list.remove_widget(todo_list_item_widget)
+    def update_list(self, todo_list):
+        self.todo_list.clear_widgets()
+        for todo_text in todo_list:
+            self.todo_list.add_widget(ToDoListItemWidget(label_text=todo_text))
 
 
 class OnetoduScreenManager(ScreenManager):
@@ -43,7 +76,19 @@ class OnetoduApp(App):
         self.title = "Onetodu"
         self.icon = "images/onetodu.png"
         self.screen_manager = OnetoduScreenManager()
+        self.todo_list_manager = TodoListManager(
+            self.screen_manager.get_screen("main"))
         return self.screen_manager
+
+    def on_start(self):
+        if DATA_FILE_PATH.parent.exists():
+            self.todo_list_manager.load_list_from_json(
+                str(DATA_FILE_PATH) + ".json")
+
+    def on_stop(self):
+        if not DATA_FILE_PATH.parent.exists():
+            DATA_FILE_PATH.parent.mkdir()
+        self.todo_list_manager.save_to_json(str(DATA_FILE_PATH) + ".json")
 
 
 def main():
